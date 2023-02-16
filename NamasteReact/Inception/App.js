2fb0130ef34a0237cef56c99eda50f92e0e89975
charanreddy404 <charanlucky6143@gmail.com>
2023-02-16 10:53:24 +0530

    first commit

---
+++ NamasteReact/Inception/App.js
@@ -0,0 +1,13 @@
+const heading = React.createElement('h1', { id: 'title' }, 'Heading 1');
+const heading2 = React.createElement('h2', { id: 'title' }, 'Heading 2');
+
+const container = React.createElement('div', { id: 'container' }, [
+  heading,
+  heading2,
+]);
+
+console.log(container);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(container);
